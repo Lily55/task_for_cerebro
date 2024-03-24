ea4c47bc7162e48aeac63dee9bb27c88e153f96e
Cheshire_fox <scarmasha@gmail.com>
2024-03-24 23:21:05 +0300

    The task done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import $ from 'jquery';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    );
 }
 
-export default App;
+function Header() {
+    return (
+      <div className='title'><h1>Posts</h1></div>
+    )
+}
+
+function Content() {
+  const defaultValue = [];
+  const [posts, setPosts] = useState(defaultValue);
+
+  const getPosts = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/posts"
+    ).then((response) => response.json());
+
+    setPosts(response);
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  const [users, setUsers] = useState(defaultValue);
+
+  const getUsers = async () => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users"
+    ).then((response) => response.json());
+
+    setUsers(response);
+  }
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  function checkUser( userId ){
+    var user = null;
+      user = users.find((user) => {return (user.id === userId)})
+      return user.username
+  }
+
+  return(
+    <div>
+      {
+        posts.map((post) => (
+          <div className='vertical-container'>
+            <tr>User: {checkUser(post.userId)}</tr>
+            <tr>Title: {post.title}</tr>
+            <tr>Body: {post.body}</tr>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
